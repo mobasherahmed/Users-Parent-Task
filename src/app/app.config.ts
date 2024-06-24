5ae4e4b5f76a2d4c55f68e18680f2d739073fe25
Ahmed Mobasher <amobasher@netsync.com>
2024-06-24 17:53:52 +0300

    add project structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient(),
+    importProvidersFrom(ToastrModule.forRoot(
+      {timeOut: 3000, positionClass: 'toast-top-right', preventDuplicates: true}))
+    
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./auth/components/login/login.component').then(m => m.LoginComponent) },
+    { path: 'users', loadComponent: () => import('./users/pages/home/home.component').then(m => m.HomeComponent), canActivate: [AuthGuard] },
+    { path: 'user/:id', loadComponent: () => import('./users/pages/user/user.component').then(m => m.UserComponent), canActivate: [AuthGuard] },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
