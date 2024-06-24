873c931a7c53753bfb06e426a00e5ad4efbb4f71
Ahmed Mobasher <amobasher@netsync.com>
2024-06-24 17:59:11 +0300

    create shared comonents

---
+++ src/app/shared/components/credits/credits.component.ts
@@ -0,0 +1,46 @@
+
+import { Component, EventEmitter, Inject, Output, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+
+@Component({
+  selector: 'app-credits',
+  templateUrl: './credits.component.html',
+  styleUrls: ['./credits.component.scss'],
+  standalone: true,
+  imports: [],
+})
+export class CreditsComponent {
+  @Output()
+  closeCreditsScreen: EventEmitter<boolean> = new EventEmitter<boolean>();
+  private router = inject(Router);
+  public authService = inject(AuthService);
+
+  animateClose: boolean = false;
+
+  links = [
+    {
+      path: 'https://github.com/mobasherahmed/Users-Parent-Task',
+      name: 'Visit git repo',
+    }
+  ];
+
+  logOut(): void {
+    localStorage.removeItem('LoggedUser');
+    this.router.navigate(['']);
+    this.handleClose();
+  }
+
+  handleClose(): void {
+
+    this.animateClose = true;
+    /**
+     * A timeout is added to execute the animation
+     * at the closing of the screen
+     */
+    setTimeout(() => {
+      this.animateClose = false;
+      this.closeCreditsScreen.emit(false);
+    }, 200);
+  }
+}
