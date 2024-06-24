da4cd73659bea19b08fa27c96656ff39fca4692a
Ahmed Mobasher <amobasher@netsync.com>
2024-06-24 17:56:30 +0300

    add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    base_url: 'https://reqres.in',
+    login_endpoint: '/api/login',
+    user_endpoint: '/api/users',
+    catalogs_endpoint: '/api/catalogs',
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    base_url: 'https://reqres.in',
+    login_endpoint: '/api/login',
+    user_endpoint: '/api/users',
+    catalogs_endpoint: '/api/catalogs',
+  };
+  
